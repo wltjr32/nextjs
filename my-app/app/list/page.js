7537c5fb76cb926302ceb38a1e1134dff1ec356e
wltjr32 <fcjs9@naver.com>
2024-07-23 15:33:49 +0900

    7_23

---
+++ my-app/app/list/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div>
+            <h4 className="title">상품 목록</h4>
+            <div className="food">
+                <h4>상품 1 14000원</h4>
+            </div>
+            <div></div>
+        </div>
+    )
+}
---
+++ my-app/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  let name = 'kim min jae'
+  return (
+    <div>
+      <h4 className="title" style={{color:'red'}}>충주상고</h4>
+      <p className="title-sub">by dev lee</p>
+    </div>
+  );
+}
